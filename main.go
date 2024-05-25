cbc4c74271f70986acff4eabf8316f705fb5bc16
MiaoJingyi <miaojingyi86@gmail.com>
2024-05-25 13:45:43 +0800

    .

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"image"
 	"image/color"
@@ -55,16 +56,18 @@ func PNGToHD() error {
 			return err
 		}
 
+		dir, name := filepath.Split(path)
+
 		if !strings.EqualFold(strings.ToLower(filepath.Ext(path)), ".png") {
 			return nil
 		}
 
-		if strings.HasSuffix(strings.ToLower(path), "_src.png") || strings.HasSuffix(strings.ToLower(path), "_dst.png") {
+		if strings.HasPrefix(strings.ToLower(name), "src_") || strings.HasPrefix(strings.ToLower(name), "dst_") {
 			return nil
 		}
 
 		// 拷贝1份原始文件
-		srcPath := strings.ReplaceAll(path, ".png", "_src.png")
+		srcPath := fmt.Sprintf("%ssrc_%s", dir, name)
 		if err := copyFile(path, srcPath); err != nil {
 			return err
 		}
@@ -84,54 +87,96 @@ func PNGToHD() error {
 		}
 
 		bounds := srcImage.Bounds()
-		log.Printf("%q : Width is %d,Height is %d \n", path, bounds.Dx(), bounds.Dy())
-		for x := 0; x < bounds.Dx(); x++ {
-			for y := 0; y < bounds.Dy(); y++ {
+		for x := 0; x < bounds.Dx(); x = x + 2 {
+			for y := 0; y < bounds.Dy(); y = y + 2 {
+				r1, g1, b1, a1 := srcImage.(*image.NRGBA).At(x, y).RGBA()
+				r2, g2, b2, a2 := srcImage.(*image.NRGBA).At(x+1, y).RGBA()
+				r3, g3, b3, a3 := srcImage.(*image.NRGBA).At(x, y+1).RGBA()
+				r4, g4, b4, a4 := srcImage.(*image.NRGBA).At(x+1, y+1).RGBA()
 
-				//r1, g1, b1, a1 := originalImage.(*image.NRGBA).At(x-1, y-1).RGBA()
-				//r2, g2, b2, a2 := originalImage.(*image.NRGBA).At(x, y-1).RGBA()
-				//r3, g3, b3, a3 := originalImage.(*image.NRGBA).At(x+1, y-1).RGBA()
-				//
-				//r4, g4, b4, a4 := originalImage.(*image.NRGBA).At(x-1, y).RGBA()
-				//r5, g5, b5, a5 := originalImage.(*image.NRGBA).At(x, y).RGBA()
-				//r6, g6, b6, a6 := originalImage.(*image.NRGBA).At(x+1, y).RGBA()
-				//
-				//r7, g7, b7, a7 := originalImage.(*image.NRGBA).At(x-1, y+1).RGBA()
-				//r8, g8, b8, a8 := originalImage.(*image.NRGBA).At(x, y+1).RGBA()
-				//r9, g9, b9, a9 := originalImage.(*image.NRGBA).At(x+1, y+1).RGBA()
-				//
-				//if (a5>>8 == 86) || (a5>>8 == 171) {
-				//	// 86 和 171 分别为残影的透明度
-				//	continue
-				//}
-				//
-				//a := (a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9) / 9
-				//if a == 0 {
-				//	continue
-				//}
-				//
-				//r := (r1 + r2 + r3 + r4 + r5 + r6 + r7 + r8 + r9) / 9
-				//g := (g1 + g2 + g3 + g4 + g5 + g6 + g7 + g8 + g9) / 9
-				//b := (b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9) / 9
-				//
-				//if a >= 0xffff*3/4 {
-				//	r, g, b, a = r5, g5, b5, a5
-				//} else if a < 0xffff*1/4 {
-				//	r, g, b, a = color.Transparent.RGBA()
-				//}
-				//
-				newRGBA := color.RGBA{
-					R: 255,
-					G: 0,
-					B: 0,
-					A: 255,
+				rgba1 := r1>>8<<24 | g1>>8<<16 | b1>>8<<8 | a1>>8
+				rgba2 := r2>>8<<24 | g2>>8<<16 | b2>>8<<8 | a2>>8
+				rgba3 := r3>>8<<24 | g3>>8<<16 | b3>>8<<8 | a3>>8
+				rgba4 := r4>>8<<24 | g4>>8<<16 | b4>>8<<8 | a4>>8
+
+				colorsMap := make(map[uint32]int)
+				colorsMap[rgba1]++
+				colorsMap[rgba2]++
+				colorsMap[rgba3]++
+				colorsMap[rgba4]++
+
+				var exists bool
+
+				for c := range colorsMap {
+					r, g, b := uint8((c&0xff000000)>>24), uint8((c&0x00ff0000)>>16), uint8((c&0x0000ff00)>>8)
+					if r>>4 == g>>4 && r>>4 == b>>4 && r>>4 == 0x0f {
+						exists = true
+					}
 				}
 
+				var r, g, b, a uint8
+
+				if exists {
+					r, g, b, a = 0xff, 0xff, 0xff, 0xff
+				} else {
+					switch len(colorsMap) {
+					case 4:
+						// 四种颜色
+						r, g, b, a = uint8(r4>>8), uint8(g4>>8), uint8(b4>>8), uint8(a4>>8)
+					case 3:
+						// 三种颜色，必有一种相同
+						for c, n := range colorsMap {
+							if n != 2 {
+								continue
+							}
+
+							r, g, b, a = uint8(c&0xff000000>>24), uint8(c&0x00ff0000>>16), uint8(c&0x0000ff00>>8), uint8(c&0x000000ff)
+						}
+					case 2:
+						// 两种颜色，可能是A颜色1个和B颜色3个，或者是A颜色2个和B颜色2个
+						colorsSlice := make([]int, 0, 2)
+						for _, count := range colorsMap {
+							colorsSlice = append(colorsSlice, count)
+						}
+
+						if colorsSlice[0] == colorsSlice[1] {
+							// A颜色2个和B颜色2个
+							r, g, b, a = uint8(r4>>8), uint8(g4>>8), uint8(b4>>8), uint8(a4>>8)
+						} else {
+							// A颜色1个和B颜色3个
+							for c, n := range colorsMap {
+								if n != 3 {
+									continue
+								}
+
+								r, g, b, a = uint8((c&0xff000000)>>24), uint8((c&0x00ff0000)>>16), uint8((c&0x0000ff00)>>8), uint8(c&0x000000ff)
+							}
+						}
+					case 1:
+						// 一种颜色
+						r, g, b, a = uint8(r4>>8), uint8(g4>>8), uint8(b4>>8), uint8(a4>>8)
+					default:
+						r, g, b, a = 0, 0, 0, 0
+					}
+				}
+
+				newRGBA := color.RGBA{R: r, G: g, B: b, A: a}
+
+				srcImage.(*image.NRGBA).Set(x/2, y/2, newRGBA)
+			}
+		}
+		for x := 0; x < bounds.Dx(); x++ {
+			for y := 0; y < bounds.Dy(); y++ {
+				if x < bounds.Dx()/2 && y < bounds.Dy()/2 {
+					continue
+				}
+
+				newRGBA := color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0x00}
 				srcImage.(*image.NRGBA).Set(x, y, newRGBA)
 			}
 		}
 
-		dstPath := strings.ReplaceAll(path, ".png", "_dst.png")
+		dstPath := fmt.Sprintf("%sdst_%s", dir, name)
 		dstFile, err := os.Create(dstPath)
 		if err != nil {
 			return err
@@ -144,6 +189,8 @@ func PNGToHD() error {
 		if err := dstFile.Close(); err != nil {
 			return err
 		}
+
+		log.Printf("%q Deal Finished ... \n", path)
 
 		return nil
 	})
