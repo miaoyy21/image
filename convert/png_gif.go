1777f5778507b3a62dc5fe5c801dad2c3d954b42
MiaoJingyi <miaojingyi86@gmail.com>
2024-05-25 16:17:37 +0800

    .

--- convert/png_gif.go
+++ convert/png_gif.go
@@ -1,9 +1,18 @@
 package convert
 
 import (
+	"fmt"
+	"github.com/xyproto/palgen"
+	"image"
+	"image/draw"
+	"image/gif"
+	"image/png"
 	"io/fs"
 	"log"
+	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -15,23 +24,97 @@ func PNGToGIF() error {
 			return err
 		}
 
-		pngName := info.Name()
-
 		if !strings.EqualFold(strings.ToLower(filepath.Ext(path)), ".png") {
 			return nil
 		}
 
-		if strings.HasPrefix(strings.ToLower(pngName), "src_") || strings.HasPrefix(strings.ToLower(pngName), "dst_") {
+		if !strings.HasPrefix(strings.ToLower(info.Name()), "dst_") {
 			return nil
 		}
 
-		pngNames = append(pngNames, pngName)
+		pngNames = append(pngNames, path)
 		return nil
 	}); err != nil {
 		return err
 	}
 
-	log.Println(pngNames)
+	gifNames := make(map[string][]string)
+	for _, pngName := range pngNames {
+		names := strings.Split(pngName, "_")
+
+		gifName := fmt.Sprintf("%s.gif", strings.Join(names[:len(names)-1], "_"))
+		pngs, ok := gifNames[gifName]
+		if !ok {
+			pngs = make([]string, 0)
+		}
+
+		pngs = append(pngs, pngName)
+
+		// PNG图片排序
+		sort.Slice(pngs, func(i, j int) bool {
+			iIndex, err := strconv.Atoi(pngs[i][strings.LastIndex(pngs[i], "_")+1 : len(pngs[i])-4])
+			if err != nil {
+				log.Panic(err)
+			}
+
+			jIndex, err := strconv.Atoi(pngs[j][strings.LastIndex(pngs[j], "_")+1 : len(pngs[j])-4])
+			if err != nil {
+				log.Panic(err)
+			}
+
+			return iIndex < jIndex
+		})
+
+		gifNames[gifName] = pngs
+	}
+
+	for gifName, pngNames := range gifNames {
+		log.Printf("GIF[ %s ] :: PNG[ %s ] ...\n", gifName, strings.Join(pngNames, ","))
+
+		out := gif.GIF{LoopCount: len(pngNames)}
+		for index, pngName := range pngNames {
+			pngFile, err := os.Open(pngName)
+			if err != nil {
+				return err
+			}
+
+			pngImage, err := png.Decode(pngFile)
+			if err != nil {
+				return err
+			}
+
+			palPalette, err := palgen.Generate(pngImage, 256)
+			if err != nil {
+				return err
+			}
+
+			paletteImg := image.NewPaletted(pngImage.Bounds(), palPalette)
+
+			draw.Draw(paletteImg, pngImage.Bounds(), pngImage, image.ZP, draw.Over)
+
+			out.Image = append(out.Image, paletteImg)
+			out.Delay = append(out.Delay, 15)
+
+			if err := pngFile.Close(); err != nil {
+				return err
+			}
+
+			log.Printf("GIF[ %s ] Dealing:: [ %d/%d ] ...\n", gifName, index, len(pngNames))
+		}
+
+		gifFile, err := os.Create(gifName)
+		if err != nil {
+			return err
+		}
+
+		if err := gif.EncodeAll(gifFile, &out); err != nil {
+			return err
+		}
+
+		if err := gifFile.Close(); err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,16 @@ module x
 
 go 1.19
 
-require github.com/u2takey/ffmpeg-go v0.5.0
+require (
+	github.com/u2takey/ffmpeg-go v0.5.0
+	github.com/xyproto/palgen v1.6.0
+)
 
 require (
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/peterhellberg/gfx v0.0.0-20230908181254-9885a9f73abe // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
+	github.com/xyproto/burnpal v1.0.0 // indirect
 )
