606e0651a827ecd2fd9443bea8454d99d05f6b01
MiaoJingyi <miaojingyi86@gmail.com>
2024-05-29 15:42:20 +0800

    .

---
+++ convert/remove_blue.go
@@ -0,0 +1,86 @@
+package convert
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func RemoveBlue() error {
+	return filepath.Walk("images", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		dir, name := filepath.Split(path)
+
+		if !strings.EqualFold(strings.ToLower(filepath.Ext(path)), ".png") {
+			return nil
+		}
+		//
+		//if !strings.Contains(strings.ToLower(name), "_1.png") {
+		//	return nil
+		//}
+
+		orgFile, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		// 原始图像
+		orgImage, _, err := image.Decode(orgFile)
+		if err != nil {
+			return err
+		}
+
+		if err := orgFile.Close(); err != nil {
+			return err
+		}
+
+		bounds := orgImage.Bounds()
+
+		// 目标图像
+		dstImage := image.NewNRGBA(
+			image.Rectangle{
+				Min: image.Point{X: 0, Y: 0},
+				Max: image.Point{X: bounds.Dx(), Y: bounds.Dy()},
+			},
+		)
+
+		for x := 0; x < bounds.Dx(); x++ {
+			for y := 0; y < bounds.Dy(); y++ {
+				r, g, b, a := orgImage.(*image.NRGBA).At(x, y).RGBA()
+				if b>>12 >= 12 && r>>12 <= 6 && g>>12 <= 6 {
+					dstImage.SetNRGBA(x, y, color.NRGBA{R: 0, G: 0, B: 0, A: 0})
+				} else {
+					newRGBA := color.NRGBA{R: uint8(r >> 8), G: uint8(g >> 8), B: uint8(b >> 8), A: uint8(a >> 8)}
+					dstImage.SetNRGBA(x, y, newRGBA)
+				}
+			}
+		}
+
+		dstPath := fmt.Sprintf("%sdst_%s", dir, name)
+		dstFile, err := os.Create(dstPath)
+		if err != nil {
+			return err
+		}
+
+		if err := png.Encode(dstFile, dstImage); err != nil {
+			return err
+		}
+
+		if err := dstFile.Close(); err != nil {
+			return err
+		}
+
+		log.Printf("%q Deal Finished ... \n", path)
+
+		return nil
+	})
+}
--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	// MP4 转为 GIF
-	if err := convert.MP4ToGIF(); err != nil {
-		log.Printf("MP4ToGIF() Failure :: %s", err.Error())
-		return
-	}
+	//if err := convert.MP4ToGIF(); err != nil {
+	//	log.Printf("MP4ToGIF() Failure :: %s", err.Error())
+	//	return
+	//}
 
 	// 合并像素点
 	//if err := convert.PNGToHD(); err != nil {
@@ -19,8 +19,14 @@ func main() {
 	//}
 
 	// 将 PNG 转为GIF
-	if err := convert.PNGToGIF(); err != nil {
-		log.Printf("PNGToGIF() Failure :: %s", err.Error())
+	//if err := convert.PNGToGIF(); err != nil {
+	//	log.Printf("PNGToGIF() Failure :: %s", err.Error())
+	//	return
+	//}
+
+	if err := convert.RemoveBlue(); err != nil {
+		log.Printf("PNGToHD() Failure :: %s", err.Error())
 		return
 	}
+
 }
